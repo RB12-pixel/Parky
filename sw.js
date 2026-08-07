a8c965e37936ce0c9cae7518ae5f2b1f4f2e710c
RB12-pixel <beluricky92@gmail.com>
2026-08-07 16:59:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+const CACHE = 'parky-v2'; // ho cambiato v1 -> v2 così si aggiorna
+
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('parky-v1').then(cache => {
+    caches.open(CACHE).then(cache => {
       return cache.addAll([
         './',
         './index.html',
@@ -10,10 +12,21 @@ self.addEventListener('install', e => {
       ]);
     })
   );
+  self.skipWaiting(); // forza aggiornamento
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
   );
+});
+
+// NUOVO: QUANDO CLICCHI LA NOTIFICA
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('index.html')); // apre l'app
 });
