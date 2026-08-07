2c03fcf554cf3e4dc4dbbf952f5f165d1cf94d1e
RB12-pixel <beluricky92@gmail.com>
2026-08-07 17:56:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,21 @@
-const CACHE = 'parky-v5'; // CAMBIA VERSIONE
+const CACHE = 'parky-v3';
+const FILES = ['./','./index.html','./manifest.json'];
 
-self.addEventListener('install', e => {e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json']))); self.skipWaiting();});
-self.addEventListener('activate', e => {e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))); self.clients.claim();});
-self.addEventListener('notificationclick', e => {e.notification.close(); e.waitUntil(clients.openWindow('./'));});
-self.addEventListener('fetch', e => {e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));});
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
 
-// QUESTO È OBBLIGATORIO PER LE ALARM
-self.addEventListener('trigger', e => {
-  if (e.name === 'showTriggeredNotification') {
-    e.waitUntil(self.registration.showNotification(e.notification.title, e.notification.options));
-  }
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./'));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
