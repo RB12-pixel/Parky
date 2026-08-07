08a8562a9936569edf544b2a4737b6879178fc07
RB12-pixel <beluricky92@gmail.com>
2026-08-07 17:47:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,13 @@
-const CACHE = 'parky-v4';
-const FILES = ['./','./index.html','./manifest.json'];
+const CACHE = 'parky-v5'; // CAMBIA VERSIONE
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
+self.addEventListener('install', e => {e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html','./manifest.json']))); self.skipWaiting();});
+self.addEventListener('activate', e => {e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))); self.clients.claim();});
+self.addEventListener('notificationclick', e => {e.notification.close(); e.waitUntil(clients.openWindow('./'));});
+self.addEventListener('fetch', e => {e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));});
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))));
-  self.clients.claim();
-});
-
-// QUESTO FA APRIRE L'APP QUANDO CLICCHI LA NOTIFICA
-self.addEventListener('notificationclick', e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow('./'));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+// QUESTO È OBBLIGATORIO PER LE ALARM
+self.addEventListener('trigger', e => {
+  if (e.name === 'showTriggeredNotification') {
+    e.waitUntil(self.registration.showNotification(e.notification.title, e.notification.options));
+  }
 });
